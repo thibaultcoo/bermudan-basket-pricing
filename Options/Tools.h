8349ace35e7235a95cd74deca5471cd890131c5a
Thibault Collin <99095146+thibaultcoo@users.noreply.github.com>
2024-05-03 17:55:56 +0200

    Delete Options directory

--- Options/Tools.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <eigen-3.4.0/Eigen/Dense>
-#include <vector>
-
-/* HELPER FUNCTIONS */
-
-double norm_cdf(double x);
-double BS_Call(double spot, double strike, double volatility, double maturity, double rate);
-double compute_expected_value_control_variate(std::vector<double> spots, std::vector<double> weights, double strike, double rate, Eigen::MatrixXd corrMatrix, double maturity);
