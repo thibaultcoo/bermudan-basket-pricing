d3f135553ee164baf44c93d381bfef0000eb5dc3
Thibault Collin <99095146+thibaultcoo@users.noreply.github.com>
2024-05-04 16:25:25 +0200

    Delete MainPricer directory

--- MainPricer/RandomGenerators/UniformGenerator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "RandomGenerator.h"
-
-class UniformGenerator : public RandomGenerator
-{
-	public:
-		UniformGenerator();
-};
--- MainPricer/SDE/BlackScholes.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "BlackScholes.h"
-
-BlackScholes::BlackScholes() { }
-
-BlackScholes::BlackScholes(RandomGenerator* generator, std::vector<double> spots, std::vector<double> rates, Eigen::MatrixXd corrMatrix, int dimension)
-	: StochasticProcess(generator, dimension), spots(spots), rates(rates), corrMatrix(corrMatrix) { }
